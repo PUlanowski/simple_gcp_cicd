00211129ba1d5073035eac62291816dec0511171
Piotr Ulanowski <46483137+PUlanowski@users.noreply.github.com>
2022-09-16 15:17:23 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http')
 const server = http.createServer((req, res) => {
     console.log('New connection')
-    res.end('Hello even more stupid stolen project 2nd commit!')
+    res.end('Hello 3rd commit!!!!!!!')
 })
 const PORT = process.env.PORT || 8080
 server.listen(PORT, () => console.log('Listening'))
